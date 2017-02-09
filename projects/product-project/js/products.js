385a391ce35cb47fdbe4137d717b49559bfb61ea
Jay Frank <jayfrank0528@gmail.com>
2017-02-09 04:20:15 +0000

    modal and search bar almost work. the modal puts the clicked item on the screen and the search doesn't do anything on the screen, but it does inside the element console

--- projects/product-project/js/products.js
+++ projects/product-project/js/products.js
@@ -17,73 +17,72 @@ $(function() {
                             .attr('id', 'image' + index)
                             .attr('data-toggle', 'modal')
                             .attr('data-target', '#myModal')
-                        ))
+                        )).click(function(){
+                            console.log(index)
+                            var modalTitle = $('#myModalLabel').text(productVal.desc)
+                            var modalImage = $('.col-md-3').append($('<img>').attr('src', 'img/product/thumbs/' + productVal.image))
+                            var modalInfo = $('.col-md-7').append($('<p>').text('$' + productVal.price.toFixed(2)))
+                                                            .append($('<p>').text('Specs: \n' + (_.map(productVal.specs, function(specVal, index){return specVal}))))
+                                                            .append($('<p>').text('Stock:' + productVal.stock))
+                            var modalColors = $('.col-md-2').append($('<p>').text('Available Colors:' + (_.map(productVal.availableColors, function(colorVal, index){return colorVal + '\n'}))))
+                            // var modalClose = function(){
+                            //     return $('.close').modal(hide)
+                            // }
+                            
+                        })
                     .append($('<div>').attr('class', 'col-md-9')
                         .append($('<h3>').text(productVal.desc))
                         .append($('<h3>').text('$' + productVal.price.toFixed(2))));
             })
             $('.container-fluid').append($populatedDivRows);
 
-            var $modals = _.map(product, function(productVal, index) {
-            console.log('ARGGGGG!!!!!')    
-            // return function(){
-                return $('<div>').attr('class', 'modal fade')
-                                .attr('id', 'myModal')
-                                .attr('tabindex', '-1')
-                                .attr('role', 'dialog')
-                                .attr('aria-labelledby', 'gridSystemModalLabel')
-                        .append($('<div>')
-                                .attr('class', 'modal-dialog')
-                                .attr('role', 'document')
-                        .append($('<div>')
-                                .attr('class', 'modal-content')
-                        .append($('<div>')
-                                .attr('class', 'modal-header')
-                        .append($('<button>')
-                                .attr('type', 'button')
-                                .attr('class', 'close')
-                                .attr('data-dismiss', 'modal')
-                                .attr('aria-label', 'Close')
-                                .attr('src', 'img' + index)
-                        .append($('<span>')
-                                .attr('aria-hidden', 'true')
-                                .text(`&times;`)))
-                        .append($('<h3>')
-                                .text(productVal.desc)))
-                        .append($('<div>')
-                                .attr('class', 'modal-body')
-                        .append($('<div>')
-                                .attr('class', 'col-md-3')
-                        .append($('<img>')
-                                .attr('src', 'img/product/thumbs/' + productVal.image)))
-                        .append($('<div>')
-                                .attr('class', 'col-md-6')
-                        .append($("<p>").text(productVal.desc))
-                        .append($("<p>").text(productVal.price))
-                        .append($("<p>").text(_.map(productVal.specs, function(specVal, index){return specVal + '\n'})))
-                        .append($("<p>").text(productVal.stock)))
-                        .append($('<div>').attr('class', 'col-md-3')
-                        .append($('<p>').text(_.map(productVal.availableColors, function(colorVal, index){return colorVal + '\n'})))))
-                        .append($('<div>').attr('class', 'modal-footer')
-                        .append($('<button>').attr('type', 'button')
-                                .attr('class', 'btn btn-default')
-                                .attr('data-dismiss', 'modal')
-                                .text('Close')))))
+        // $('#searchBtn').on('click', function(){
+        //     console.log($('#searchBar').val())
+        // })
+        //  $('#searchBar').on('keyup', function(){
+        //     console.log($('#searchBar').val())
+        //     _.filter(product, function(value, index) {
+        //         if($('#searchBar').val().toLowerCase() == value){
+        //             return value
+        //         }
+        //     })
+        // })    
+        
+        
+        
+        var $ul = $('<ul>').attr('class', 'live-search-list').appendTo('.main');
+            
+        
+        var $li = _.map(product, function(val, index){
+           var type = $('<li>').attr('class', 'live-search-list li').text(val.type)
+           var desc = $('<li>').attr('class', 'live-search-list li').text(val.desc)
+           var price = $('<li>').attr('class', 'live-search-list li').text(val.price)
+            var color = $('<li>').attr('class', 'live-search-list li').text(val.color)
+            var stock = $('<li>').attr('class', 'live-search-list li').text(val.stock)
+           return type, desc, color
+        })
+        $($ul).append($li)
+        
+        
+        $('.live-search-list li').each(function(){
+           $(this).attr('data-search-term', $(this).text().toLowerCase());
+        });
 
-            // }
-            })
-            
-            $('body').append($modals);
+         $('.live-search-box').on('keyup', function(){
 
-            //  $('#myModal').modal(show)
-            // $('#image0').on('click', function(){
-                // console.log('dsvfdskjvjd')
-            // } )
-            // $('#myModal').on('shown.bs.modal', function () {
-            //   $('#myInput').focus()
-            // })
+            var searchTerm = $(this).val().toLowerCase();
             
+                $('.live-search-list li').each(function(){
             
+                    if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
+                        $(this).show();
+                    } else {
+                        $(this).hide();
+                    }
+            
+                });
+            
+            });
         })
         // ALL YOUR CODE GOES ABOVE HERE //
 });
